34cdb6bcc595cb56d8b5e1677d25baabe0297e21
Chris <chrisadebayo@googlemail.com>
2026-05-18 17:15:18 +0100

    Have added the _stg_business.yml file and modified the stg_orders.sql file to include the order_status field and to include different tests.

--- models/Staging/stg_orders.sql
+++ models/Staging/stg_orders.sql
@@ -2,6 +2,6 @@ select
         id as order_id,
         user_id as customer_id,
         order_date,
-        status,
+        status as order_status,
         _etl_loaded_at
     from  {{ source('raw_files', 'orders') }}
